dcb814df93f6c561a7c82822f19149a5e25b4b27
Ajmayen Fayek <ajmayenfayek999999@gmail.com>
2025-10-12 14:22:18 +0600

    Initial commit

---
+++ src/main/java/com/ajmayen/softwaregazeportal/SoftwareGazePortalApplication.java
@@ -0,0 +1,13 @@
+package com.ajmayen.softwaregazeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareGazePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareGazePortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajmayen/softwaregazeportal/SoftwareGazePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajmayen.softwaregazeportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareGazePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
