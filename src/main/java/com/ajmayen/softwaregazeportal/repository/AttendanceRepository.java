5bd705fb8b9de48928b921a4489f9e3241c50009
Ajmayen Fayek <ajmayenfayek999999@gmail.com>
2025-11-11 10:21:30 +0600

    Added Attendance Repository

---
+++ src/main/java/com/ajmayen/softwaregazeportal/repository/AttendanceRepository.java
@@ -0,0 +1,14 @@
+package com.ajmayen.softwaregazeportal.repository;
+
+import com.ajmayen.softwaregazeportal.model.Attendance;
+import com.ajmayen.softwaregazeportal.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+
+    List<Attendance> findAllByPunchInBetween(LocalDateTime start, LocalDateTime end);
+    List<Attendance> findAllByEmployeeAndPunchInBetween(User employee, LocalDateTime start, LocalDateTime end);
+}
