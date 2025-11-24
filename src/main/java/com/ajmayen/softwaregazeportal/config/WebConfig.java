a6a920a7c22e94a8a5e6b5a29e540886b23769c0
Ajmayen Fayek <ajmayenfayek999999@gmail.com>
2025-11-24 19:13:03 +0600

    Added Environment variable file for deployment

--- src/main/java/com/ajmayen/softwaregazeportal/config/WebConfig.java
+++ src/main/java/com/ajmayen/softwaregazeportal/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.ajmayen.softwaregazeportal.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                .allowedOrigins(frontendUrl, "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
